a69cdaf5f9da12ee52881ef7cc759fa2e8b50cca
Anuj Raval <Anujraval24@gmail.com>
2023-06-27 01:36:17 +0530

    learning python basics - day 01

---
+++ main.py
@@ -0,0 +1,91 @@
+# ============== #
+## variable defining
+# first_name = "Anuj" # string
+# last_name = "Raval" # string
+# age = 27 # number
+# phone_number = "+919825098250" # string
+# ============== #
+
+# ============== #
+## print function + input function
+# print("Here is the Information of")
+# print("Name", first_name, last_name) # Print
+# print("Age", age)
+# print("Phone Number", phone_number)
+# is_verified = input("Are you verified? ") # User Inputs
+# print("isVerified", is_verified)
+# ============== #
+
+
+# ============== #
+## type conversion (int, str, bool, float)
+
+# birth_day = input("What's your birth day? ")
+# birth_month = input("What's your birth month? ")
+# birth_year = input("What's your birth year? ")
+
+# print("Birth Day", int(birth_day), int(birth_month) , int(birth_year))
+# ============== #
+
+# ============== #
+## Exercise - Sum
+# print("Sum Calculator")
+
+# first_digit = float(input("Enter First Digit "))
+# second_digit =float( input("Enter Second Digit "))
+
+# print("Total :" , first_digit + second_digit)
+# ============== #
+
+
+# ============== #
+## strings
+name = "anuj raval"
+# name = name.capitalize()
+# print("Name", name) 
+
+# found_str_idx = name.find("j")
+# print("Returns index of found character", found_str_idx) 
+
+# print("Count of ", "a in " ,name, name.count("a"))
+
+# print("Has Raval? ", "raval" in name)
+# ============== #
+
+
+# ============== #
+## Arithmetic Operations
+# x = 5
+# y = 6
+# x *=y
+# print(x) # (/ * + -) , (+= -= *= /=)
+# ============== #
+
+# ============== #
+## Operator Precedence
+# x = 5
+# y = 6
+# z= 10
+# x = (x + y) * z
+# print(x) 
+# ============== #
+## Comparison Operator (>, <, >=, <=, ==, !=)
+# ============== #
+## Logical Operator (and, or, not)
+# ============== #
+## IF Statements (if, elif)
+is_verified = True
+if  is_verified:
+    print("User is verified")
+elif not is_verified: 
+    print("User is not verified")
+else:
+    print("User does not exists")
+# ============== #
+# ============== #
+# ============== #
+# ============== #
+# ============== #
+# ============== #
+# ============== #
+# ============== #
